78ae758915ac9a4551ef67c8c014148016f6c88d
srl <srl@251d0590-4201-4cf1-90de-194747b24ca1>
2006-12-01 18:06:15 +0000

    ticket:2401: r20715 => tags/post-cvs2svn-cleanedup

---
+++ icuapps/tags/post-cvs2svn-cleanedup/locexp/util/unicode/translitcb.h
@@ -0,0 +1,54 @@
+/**********************************************************************
+*   Copyright (C) 1999-2006, International Business Machines
+*   Corporation and others.  All Rights Reserved.
+***********************************************************************/
+/*
+
+
+  'TRANSLIT' callback. Uses (special) fonts to render different scripts
+  in HTML..
+
+  Steven R. Loomis <srl [at] icu-project.org>
+
+  **NONE OF THIS CODE IS MULTITHREAD SAFE [yet] **
+  **NONE OF THIS CODE IS MULTITHREAD SAFE [yet] **
+
+*/
+
+#ifndef TRANSLITCB_H
+#define TRANSLITCB_H
+
+#include "unicode/ucnv.h"
+#include "unicode/utrans.h"
+
+typedef struct
+{
+  UConverterFromUCallback  subCallback;
+
+  /* PUBLIC */
+  const void    *subContext;  /* For Chaining */
+  char locale[256];      /* source locale, will translit  locale-Latin */
+  UErrorCode     transerr;
+  UBool          html;
+
+  /* PRIVATE */
+  UConverter       *utf8; 
+  UTransliterator  *trans;
+} FromUTransliteratorContext;
+
+
+U_CAPI void 
+  UCNV_FROM_U_CALLBACK_TRANSLITERATED  (const void *context,
+                                UConverterFromUnicodeArgs *fromUArgs,
+                                const UChar* codeUnits,
+                                int32_t length,
+                                UChar32 codePoint,
+                                UConverterCallbackReason reason,
+                                UErrorCode *err);
+
+#endif
+
+
+
+
+
