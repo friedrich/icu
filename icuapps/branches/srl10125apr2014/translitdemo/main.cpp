a7bc2f9ee90543a9132098309d94a4c570d2fbee
srl <srl@251d0590-4201-4cf1-90de-194747b24ca1>
2014-04-15 00:09:27 +0000

    ticket:10125: delete branch

--- icuapps/branches/srl10125apr2014/translitdemo/main.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2000-2001 IBM, Inc. and others.
-
-#include "TransliteratorCGI.h"
-
-int main(int argc, char* argv[]) {
-    TransliteratorCGI cgi;
-    cgi.run();
-    return 0;
-}
