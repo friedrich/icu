f5ddfee4fc54e05c506e0a60558c63d2551b5442
sgill <sgill@251d0590-4201-4cf1-90de-194747b24ca1>
2015-12-14 19:42:45 +0000

    ticket:10152 Create a private branch for work on moving to junit.

---
+++ icu4j/branches/sgill/junit/main/classes/core/src/com/ibm/icu/text/QuantityFormatter.java
@@ -0,0 +1,77 @@
+/*
+ *******************************************************************************
+ * Copyright (C) 2013-2015, International Business Machines Corporation and
+ * others. All Rights Reserved.
+ *******************************************************************************
+ */
+package com.ibm.icu.text;
+
+import com.ibm.icu.impl.SimplePatternFormatter;
+import com.ibm.icu.impl.StandardPlural;
+
+/**
+ * QuantityFormatter represents an unknown quantity of something and formats a known quantity
+ * in terms of that something. For example, a QuantityFormatter that represents X apples may
+ * format 1 as "1 apple" and 3 as "3 apples" 
+ * <p>
+ * QuanitityFormatter appears here instead of in com.ibm.icu.impl because it depends on
+ * PluralRules and DecimalFormat. It is package-protected as it is not meant for public use.
+ */
+class QuantityFormatter {
+    private final SimplePatternFormatter[] templates =
+            new SimplePatternFormatter[StandardPlural.COUNT];
+
+    public QuantityFormatter() {}
+
+    /**
+     * Adds a template if there is none yet for the plural form.
+     *
+     * @param variant the plural variant, e.g "zero", "one", "two", "few", "many", "other"
+     * @param template the text for that plural variant with "{0}" as the quantity. For
+     * example, in English, the template for the "one" variant may be "{0} apple" while the
+     * template for the "other" variant may be "{0} apples"
+     * @throws IllegalArgumentException if variant is not recognized or
+     *  if template has more than just the {0} placeholder.
+     */
+    public void addIfAbsent(CharSequence variant, String template) {
+        int idx = StandardPlural.indexFromString(variant);
+        if (templates[idx] != null) {
+            return;
+        }
+        templates[idx] = SimplePatternFormatter.compileMinMaxPlaceholders(template, 0, 1);
+    }
+
+    /**
+     * @return true if this object has at least the "other" variant
+     */
+    public boolean isValid() {
+        return templates[StandardPlural.OTHER_INDEX] != null;
+    }
+
+    /**
+     * Format formats a quantity with this object.
+     * @param quantity the quantity to be formatted
+     * @param numberFormat used to actually format the quantity.
+     * @param pluralRules uses the quantity and the numberFormat to determine what plural
+     *  variant to use for fetching the formatting template.
+     * @return the formatted string e.g '3 apples'
+     */
+    public String format(double quantity, NumberFormat numberFormat, PluralRules pluralRules) {
+        String formatStr = numberFormat.format(quantity);
+        String variant = pluralRules.select(quantity, numberFormat);
+        return getByVariant(variant).format(formatStr);
+    }
+    
+    /**
+     * Gets the SimplePatternFormatter for a particular variant.
+     * @param variant "zero", "one", "two", "few", "many", "other"
+     * @return the SimplePatternFormatter
+     */
+    public SimplePatternFormatter getByVariant(CharSequence variant) {
+        assert isValid();
+        int idx = StandardPlural.indexOrOtherIndexFromString(variant);
+        SimplePatternFormatter template = templates[idx];
+        return (template == null && idx != StandardPlural.OTHER_INDEX) ?
+                templates[StandardPlural.OTHER_INDEX] : template;
+    }
+}
