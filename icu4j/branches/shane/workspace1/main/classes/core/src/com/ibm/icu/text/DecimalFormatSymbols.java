0f5c2b2c3205a6ec17647fdc76e98fb97d2cae37
shane <shane@251d0590-4201-4cf1-90de-194747b24ca1>
2016-04-28 21:57:11 +0000

    ticket:12521 DFS work in progress.

--- icu4j/branches/shane/workspace1/main/classes/core/src/com/ibm/icu/text/DecimalFormatSymbols.java
+++ icu4j/branches/shane/workspace1/main/classes/core/src/com/ibm/icu/text/DecimalFormatSymbols.java
@@ -11,7 +11,6 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Locale;
-import java.util.MissingResourceException;
 
 import com.ibm.icu.impl.CurrencyData;
 import com.ibm.icu.impl.CurrencyData.CurrencyDisplayInfo;
@@ -24,6 +23,7 @@ import com.ibm.icu.util.Currency;
 import com.ibm.icu.util.ICUCloneNotSupportedException;
 import com.ibm.icu.util.ULocale;
 import com.ibm.icu.util.ULocale.Category;
+import com.ibm.icu.impl.UResource;
 import com.ibm.icu.util.UResourceBundle;
 
 /**
@@ -841,6 +841,91 @@ public class DecimalFormatSymbols implements Cloneable, Serializable {
     }
 
     /**
+     * List of field names to be loaded from the data files.
+     * The indices of each name into the array correspond to the position of that item in the
+     * numberElements array.
+     */
+    private static final String[] SYMBOL_KEYS = {
+            "decimal",
+            "group",
+            "list",
+            "percentSign",
+            "minusSign",
+            "plusSign",
+            "exponential",
+            "perMille",
+            "infinity",
+            "nan",
+            "currencyDecimal",
+            "currencyGroup",
+            "superscriptingExponent"
+    };
+
+    /**
+     * List of default values for the symbols.
+     */
+    private static final String[] SYMBOL_DEFAULTS = new String[] {
+            ".", // decimal
+            ",", // group
+            ";", // list
+            "%", // percentSign
+            "-", // minusSign
+            "+", // plusSign
+            "E", // exponential
+            "\u2030", // perMille
+            "\u221e", // infinity
+            "NaN", // NaN
+            null, // currency decimal
+            null, // currency group
+            "\u00D7" // superscripting exponent
+        };
+
+    /**
+     * Constants for path names in the data bundles.
+     */
+    private static final String LATIN_NUMBERING_SYSTEM = "latn";
+    private static final String NUMBER_ELEMENTS = "NumberElements";
+    private static final String SYMBOLS = "symbols";
+
+    /**
+     * Sink for enumerating all of the decimal format symbols (more specifically, anything
+     * under the "NumberElements.symbols" tree).
+     *
+     * More specific bundles (en_GB) are enumerated before their parents (en_001, en, root):
+     * Only store a value if it is still missing, that is, it has not been overridden.
+     */
+    private static final class DecFmtDataSink extends UResource.Sink {
+
+        private String[] numberElements; // Array where to store the characters (set in constructor)
+
+        public DecFmtDataSink(String[] numberElements) {
+            this.numberElements = numberElements;
+        }
+
+        @Override
+        public void put(UResource.Key key, UResource.Value value, boolean noFallback) {
+            UResource.Table symbolsTable = value.getTable();
+            for (int j = 0; symbolsTable.getKeyAndValue(j, key, value); ++j) {
+                // Look up the field index.
+                int i;
+                for (i = 0; i<SYMBOL_KEYS.length; i++) {
+                    if (key.contentEquals(SYMBOL_KEYS[i])) {
+                        break;
+                    }
+                }
+
+                // If we didn't find the field name, leave now
+                if (i == SYMBOL_KEYS.length) continue;
+
+                // Set the value if null.
+                if (numberElements[i] == null) {
+                    numberElements[i] = value.toString();
+                }
+            }
+        }
+    }
+
+    /**
      * Initializes the symbols from the LocaleElements resource bundle.
      * Note: The organization of LocaleElements badly needs to be
      * cleaned up.
@@ -878,52 +963,49 @@ public class DecimalFormatSymbols implements Cloneable, Serializable {
             digits[7] = DecimalFormat.PATTERN_SEVEN_DIGIT;
             digits[8] = DecimalFormat.PATTERN_EIGHT_DIGIT;
             digits[9] = DecimalFormat.PATTERN_NINE_DIGIT;
-            nsName = "latn"; // Default numbering system
+            nsName = LATIN_NUMBERING_SYSTEM; // Default numbering system
         }
 
-        /* try the cache first */
+        // Open the resource bundle and get the locale IDs
+        // TODO: Is there a better way to get the locale than making an ICUResourceBundle instance?
+        ICUResourceBundle rb = (ICUResourceBundle)UResourceBundle.
+            getBundleInstance(ICUResourceBundle.ICU_BASE_NAME, locale);
+        // TODO: Determine actual and valid locale correctly.
+        ULocale uloc = rb.getULocale();
+        setLocale(uloc, uloc);
+
+        // Try loading from the cache
         String[][] data = cachedLocaleData.get(locale);
-        String[] numberElements;
-        if (data == null) {  /* cache miss */
-            data = new String[1][];
-            ICUResourceBundle rb = (ICUResourceBundle)UResourceBundle.
-                getBundleInstance(ICUResourceBundle.ICU_BASE_NAME, locale);
-            boolean isLatn = nsName.equals("latn");
-            String baseKey = "NumberElements/" + nsName + "/symbols/";
-            String latnKey = "NumberElements/latn/symbols/";
-            String[] symbolKeys = { "decimal", "group", "list", "percentSign", "minusSign", "plusSign", "exponential", "perMille", "infinity", "nan", "currencyDecimal", "currencyGroup", "superscriptingExponent" };
-            String[] fallbackElements = { ".", ",", ";", "%", "-", "+", "E", "\u2030", "\u221e", "NaN", null, null };
-            String[] symbolsArray = new String[symbolKeys.length];
-            for ( int i = 0 ; i < symbolKeys.length; i++ ) {
-                try {
-                    symbolsArray[i] = rb.getStringWithFallback(baseKey+symbolKeys[i]);
-                } catch (MissingResourceException ex) {
-                    if (!isLatn) { // Fall back to latn numbering system for symbols if desired symbol isn't found.
-                        try {
-                            symbolsArray[i] = rb.getStringWithFallback(latnKey+symbolKeys[i]);
-                        } catch (MissingResourceException ex1) {
-                            symbolsArray[i] = fallbackElements[i];
-                        }
-                    } else {
-                        symbolsArray[i] = fallbackElements[i];
-                    }
+        if (data == null) {
+            // Cache miss!
+            data = new String[1][SYMBOL_KEYS.length];
+
+            // Load using a data sink
+            DecFmtDataSink sink = new DecFmtDataSink(data[0]);
+            rb.getAllItemsWithFallback(NUMBER_ELEMENTS + "/" + nsName + "/" + SYMBOLS, sink);
+            resolveMissingMonetarySeparators(data[0]);
+
+            // Load the Latin fallback if necessary
+            int numNull = 0;
+            for (String entry : data[0]) if (entry == null) numNull++;
+            if (numNull > 0 && !nsName.equals(LATIN_NUMBERING_SYSTEM)) {
+                rb.getAllItemsWithFallback(NUMBER_ELEMENTS + "/" + LATIN_NUMBERING_SYSTEM + "/" + SYMBOLS, sink);
+                resolveMissingMonetarySeparators(data[0]);
+            }
+
+            // Fill in missing values
+            for (int i = 0; i < SYMBOL_KEYS.length; i++) {
+                if (data[0][i] == null) {
+                    data[0][i] = SYMBOL_DEFAULTS[i];
                 }
             }
 
-            data[0] = symbolsArray;
-            /* update cache */
+            // Save in cache
             cachedLocaleData.put(locale, data);
         }
-        numberElements = data[0];
+        String[] numberElements = data[0];
 
-        ICUResourceBundle r = (ICUResourceBundle)UResourceBundle.
-            getBundleInstance(ICUResourceBundle.ICU_BASE_NAME, locale);
-
-        // TODO: Determine actual and valid locale correctly.
-        ULocale uloc = r.getULocale();
-        setLocale(uloc, uloc);
-
-
+        // Copy data from the numberElements map into instance fields
         decimalSeparator = numberElements[0].charAt(0);
         groupingSeparator = numberElements[1].charAt(0);
         patternSeparator = numberElements[2].charAt(0);
@@ -936,25 +1018,10 @@ public class DecimalFormatSymbols implements Cloneable, Serializable {
         perMill = numberElements[7].charAt(0);
         infinity = numberElements[8];
         NaN = numberElements[9];
+        monetarySeparator = numberElements[10].charAt(0);
+        monetaryGroupingSeparator = numberElements[11].charAt(0);
+        exponentMultiplicationSign = numberElements[12];
 
-        if ( numberElements[10] != null) {
-            monetarySeparator = numberElements[10].charAt(0);
-        } else {
-            monetarySeparator = decimalSeparator;
-        }
-        
-        if ( numberElements[11] != null) {
-            monetaryGroupingSeparator = numberElements[11].charAt(0);
-        } else {
-            monetaryGroupingSeparator = groupingSeparator;
-        }
-        
-        if ( numberElements[12] != null) {
-            exponentMultiplicationSign = numberElements[12];
-        } else {
-            exponentMultiplicationSign = "\u00D7";
-        }
-        
         digit = DecimalFormat.PATTERN_DIGIT;  // Localized pattern character no longer in CLDR
         padEscape = DecimalFormat.PATTERN_PAD_ESCAPE;
         sigDigit  = DecimalFormat.PATTERN_SIGNIFICANT_DIGIT;
@@ -994,6 +1061,18 @@ public class DecimalFormatSymbols implements Cloneable, Serializable {
         currencySpcAfterSym[CURRENCY_SPC_CURRENCY_MATCH] = spcInfo.afterCurrencyMatch;
         currencySpcAfterSym[CURRENCY_SPC_SURROUNDING_MATCH] = spcInfo.afterContextMatch;
         currencySpcAfterSym[CURRENCY_SPC_INSERT] = spcInfo.afterInsert;
+    }
+
+    /**
+     * If monetary decimal or grouping were not explicitly set, then set them to be the same as
+     * their non-monetary counterparts.
+     * @param numberElements
+     */
+    private static void resolveMissingMonetarySeparators(String[] numberElements) {
+        if (numberElements[10] == null)
+            numberElements[10] = numberElements[0];
+        if (numberElements[11] == null)
+            numberElements[11] = numberElements[1];
     }
 
     /**
